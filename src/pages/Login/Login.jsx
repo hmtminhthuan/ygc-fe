0eb9af91f6776aec8823519b4b12ba84d4e8f944
NgocTram <tramngoc3402@gmail.com>
2023-05-21 22:57:25 +0700

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+
+import "./Login.scss";
+import { Link } from "react-router-dom";
+
+import video from "../../assets/video.mp4";
 
 export default function Login() {
-    return (
-        <div>Login</div>
-    )
+  return (
+    <div className="loginPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+
+          <div className="textDiv">
+            <h2 className="title">Welcome back</h2>
+            <p>abcdjlk</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Don't have account?</span>
+            <Link to={"/register"}>
+              <button className="btn">Register</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
