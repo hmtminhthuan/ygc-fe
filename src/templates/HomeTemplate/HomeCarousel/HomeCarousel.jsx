8a1c0d4ec3cfad784a56d097cfa1d95002e85ffb
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-21 20:46:07 +0700

    upload

--- src/templates/HomeTemplate/HomeCarousel/HomeCarousel.jsx
+++ src/templates/HomeTemplate/HomeCarousel/HomeCarousel.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Carousel } from 'antd';
+import './HomeCarousel.scss'
+import { SearchOutlined } from '@ant-design/icons'
 
 export default function HomeCarousel() {
     return (
-        <div>HomeCarousel</div>
+        <section className="hero-wrap js-fullheight">
+            <div className="overlay" />
+            <div className="container">
+                <div className="row no-gutters slider-text js-fullheight align-items-center justify-content-start">
+                    <div className="col-md-8 ftco-animate">
+                        <h1 className="typewrite mb-3" data-period={1000} data-type="[ &quot;Inspiration For Living.&quot;, &quot;Therapy Against Stress.&quot;, &quot;A Flexible Body.&quot;]">
+                            <span className="wrap" />
+                        </h1>
+                        <h2 className="mb-5" style={{ color: 'rgba(0, 0, 0)' }}>Do Yoga today for a better tomorrow</h2>
+
+                        <p>
+                            <a href="#" className="btn btn-primary p-3 px-4">15 Day Free Trial</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section >
+
     )
 }
