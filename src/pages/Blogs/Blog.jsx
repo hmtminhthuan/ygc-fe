ea90ddf10342c371541070b2fa44e2f1d8e54ef4
NgocTram <tramngoc3402@gmail.com>
2023-05-23 14:36:17 +0700

    style blog

--- src/pages/Blogs/Blog.jsx
+++ src/pages/Blogs/Blog.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Blog.scss";
+import "./Template.scss";
 // import Container from "react-bootstrap/Container";
 // import Nav from "react-bootstrap/Nav";
 // import Navbar from "react-bootstrap/Navbar";
@@ -16,50 +18,53 @@ function Blog() {
       {/* Sidebar (hidden by default) */}
       <nav
         className="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left"
-        style={{ display: "none", zIndex: 2, width: "40%", minWidth: 300 }}
         id="mySidebar"
       >
         <a
           href="javascript:void(0)"
-          onClick={w3_close}
+          onClick={() => w3_close()}
           className="w3-bar-item w3-button"
         >
           Close Menu
         </a>
-        <a href="#food" onClick={w3_close} className="w3-bar-item w3-button">
+        <a
+          href="#food"
+          onClick={() => w3_close()}
+          className="w3-bar-item w3-button"
+        >
           Food
         </a>
-        <a href="#about" onClick={w3_close} className="w3-bar-item w3-button">
+        <a
+          href="#about"
+          onClick={() => w3_close()}
+          className="w3-bar-item w3-button"
+        >
           About
         </a>
       </nav>
 
       {/* Top menu */}
       <div className="w3-top">
-        <div
-          className="w3-white w3-xlarge"
-          style={{ maxWidth: 1200, margin: "auto" }}
-        >
-          <div className="w3-button w3-padding-16 w3-left" onClick={w3_open}>
+        <div className="w3-white w3-xlarge">
+          <div
+            className="w3-button w3-padding-16 w3-left"
+            onClick={() => w3_open()}
+          >
             ☰
           </div>
-          <div className="w3-right w3-padding-16">Mail</div>
-          <div className="w3-center w3-padding-16">My Food</div>
+
+          <div className="w3-center w3-padding-16">Our Blog</div>
         </div>
       </div>
 
-      {/* !PAGE CONTENT! */}
-      <div
-        className="w3-main w3-content w3-padding"
-        style={{ maxWidth: 1200, marginTop: 100 }}
-      >
+      {/* PAGE CONTENT */}
+      <div className="w3-main w3-content w3-padding">
         {/* First Photo Grid */}
         <div className="w3-row-padding w3-padding-16 w3-center" id="food">
           <div className="w3-quarter">
             <img
               src="https://www.w3schools.com/w3images/sandwich.jpg"
               alt="Sandwich"
-              style={{ width: "100%" }}
             />
             <h3>The Perfect Sandwich, A Real NYC Classic</h3>
             <p>
@@ -71,7 +76,6 @@ function Blog() {
             <img
               src="https://www.w3schools.com/w3images/steak.jpg"
               alt="Steak"
-              style={{ width: "100%" }}
             />
             <h3>Let Me Tell You About This Steak</h3>
             <p>
@@ -79,23 +83,19 @@ function Blog() {
               praesent tincidunt ipsum lipsum.
             </p>
           </div>
-
           <div className="w3-quarter">
             <img
               src="https://www.w3schools.com/w3images/cherries.jpg"
               alt="Cherries"
-              style={{ width: "100%" }}
             />
             <h3>Cherries, interrupted</h3>
             <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
             <p>What else?</p>
           </div>
-
           <div className="w3-quarter">
             <img
               src="https://www.w3schools.com/w3images/wine.jpg"
               alt="Pasta and Wine"
-              style={{ width: "100%" }}
             />
             <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
             <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
@@ -108,7 +108,6 @@ function Blog() {
             <img
               src="https://www.w3schools.com/w3images/popsicle.jpg"
               alt="Popsicle"
-              style={{ width: "100%" }}
             />
             <h3>All I Need Is a Popsicle</h3>
             <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
@@ -117,7 +116,6 @@ function Blog() {
             <img
               src="https://www.w3schools.com/w3images/salmon.jpg"
               alt="Salmon"
-              style={{ width: "100%" }}
             />
             <h3>Salmon For Your Skin</h3>
             <p>
@@ -129,9 +127,8 @@ function Blog() {
             <img
               src="https://www.w3schools.com/w3images/sandwich.jpg"
               alt="Sandwich"
-              style={{ width: "100%" }}
             />
-            <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+            <h3>The Perfect Sandwich, A Real Classic</h3>
             <p>
               Just some random text, lorem ipsum text praesent tincidunt ipsum
               lipsum.
@@ -141,7 +138,6 @@ function Blog() {
             <img
               src="https://www.w3schools.com/w3images/croissant.jpg"
               alt="Croissant"
-              style={{ width: "100%" }}
             />
             <h3>Le French</h3>
             <p>
@@ -175,40 +171,42 @@ function Blog() {
           </div>
         </div>
 
+        <hr id="about" />
+
         {/* About Section */}
-        <div className="w3-content w3-container w3-padding-64">
-          <div id="about" className="w3-container w3-padding-32 w3-center">
-            <h3>About Me, The Food Man</h3>
-            <br />
-            <img
-              src="https://www.w3schools.com/w3images/chef.jpg"
-              alt="Me"
-              className="w3-image"
-              style={{ display: "block", margin: "auto" }}
-              width="800"
-              height="533"
-            />
-            <div className="w3-padding-32">
-              <h4>
-                <b>I am Who I Am!</b>
-              </h4>
-              <h6>
-                <i>With Passion For Real, Good Food</i>
-              </h6>
-              <p>
-                Just me, myself and I, exploring the universe of unknownment. I
-                have a heart of love and an interest of lorem ipsum and mauris
-                neque quam blog. I want to share my world with you. Praesent
-                tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
-                porta lectus vitae, ultricies congue gravida diam non fringilla.
-                Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
-                condimentum, porta lectus vitae, ultricies congue gravida diam
-                non fringilla.
-              </p>
-            </div>
+        <div className="w3-container w3-padding-32 w3-center">
+          <h3>About Me, The Food Man</h3>
+          <br />
+          <img
+            src="https://www.w3schools.com/w3images/chef.jpg"
+            alt="Me"
+            className="w3-image"
+            style={{ display: "block", margin: "auto" }}
+            width="800"
+            height="533"
+          />
+          <div className="w3-padding-32">
+            <h4>
+              <b>I am Who I Am!</b>
+            </h4>
+            <h6>
+              <i>With Passion For Real, Good Food</i>
+            </h6>
+            <p>
+              Just me, myself and I, exploring the universe of unknownment. I
+              have a heart of love and an interest of lorem ipsum and mauris
+              neque quam blog. I want to share my world with you.Praesent
+              tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta
+              lectus vitae, ultricies congue gravida diam non fringilla.
+              Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
+              condimentum, porta lectus vitae, ultricies congue gravida diam non
+              fringilla.
+            </p>
           </div>
         </div>
+        <hr />
 
+        {/* Footer */}
         <footer className="w3-row-padding w3-padding-32">
           <div className="w3-third">
             <h3>FOOTER</h3>
@@ -224,6 +222,7 @@ function Blog() {
               </a>
             </p>
           </div>
+
           <div className="w3-third">
             <h3>BLOG POSTS</h3>
             <ul className="w3-ul w3-hoverable">
@@ -231,7 +230,7 @@ function Blog() {
                 <img
                   src="https://www.w3schools.com/w3images/workshop.jpg"
                   className="w3-left w3-margin-right"
-                  style={{ width: 50 }}
+                  style={{ width: "50px" }}
                 />
                 <span className="w3-large">Lorem</span>
                 <br />
@@ -241,7 +240,7 @@ function Blog() {
                 <img
                   src="https://www.w3schools.com/w3images/gondol.jpg"
                   className="w3-left w3-margin-right"
-                  style={{ width: 50 }}
+                  style={{ width: "50px" }}
                 />
                 <span className="w3-large">Ipsum</span>
                 <br />
