fe65e6cba20b28c49f4f0ba41631fc58cde381c9
NgocTram <tramngoc3402@gmail.com>
2023-05-24 23:08:12 +0700

    update

--- src/pages/Blogs/Blog.jsx
+++ src/pages/Blogs/Blog.jsx
@@ -4,7 +4,8 @@ import Banner from "../../component/Banner/Banner";
 import BlogDetail from "./BlogDetail";
 import axios from "axios";
 import "./Blog.scss";
-import "./Template.scss";
+import "./BlogDetail.scss";
+
 // import Container from "react-bootstrap/Container";
 // import Nav from "react-bootstrap/Nav";
 // import Navbar from "react-bootstrap/Navbar";
@@ -23,217 +24,70 @@ function Blog() {
       });
   }, []);
 
+  // const formatDate = (dateString) => {
+  //   const dateObj = new Date(dateString);
+
+  //   const day = dateObj.getDate();
+  //   const month = dateObj.getMonth() + 1;
+  //   const year = dateObj.getFullYear();
+
+  //   return `${day}-${month}-${year}`;
+  // };
+
   return (
     <div>
       <HeaderHome />
       <Banner title={"Our Blog"} descripton={"Yoga Blog Series"} />
-      <section className="w-100 bloglist-area">
+      <section className="w-100 bloglist-area px-md-4">
         <div className="blog-contaier flex justify-content-center align-content-center">
           <div className="row">
             {blogList.map((blog, index) => {
+              // const formattedDate = formatDate(blog.date);
               return (
                 <BlogDetail
                   key={index}
                   header={blog.header}
-                  content={blog.content}
+                  // content={blog.content}
+                  // date={formattedDate}
                   firstName={blog.firstName}
-                  lastName={blog.lastName}
                 />
               );
             })}
           </div>
         </div>
       </section>
-      {/* Sidebar (hidden by default) */}
-      {/* <nav
-        className="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left"
-        id="mySidebar"
-      >
-        <a
-          href="javascript:void(0)"
-          onClick={() => w3_close()}
-          className="w3-bar-item w3-button"
-        >
-          Close Menu
-        </a>
-        <a
-          href="#food"
-          onClick={() => w3_close()}
-          className="w3-bar-item w3-button"
-        >
-          Food
-        </a>
-        <a
-          href="#about"
-          onClick={() => w3_close()}
-          className="w3-bar-item w3-button"
-        >
-          About
-        </a>
-      </nav> */}
 
-      {/* Top menu */}
-      {/* <div className="w3-top">
-        <div className="w3-white w3-xlarge">
-          <div
-            className="w3-button w3-padding-16 w3-left"
-            onClick={() => w3_open()}
-          >
-            ☰
-          </div>
-
-          <div className="w3-center w3-padding-16">Our Blog</div>
-        </div>
-      </div> */}
-
-      {/* PAGE CONTENT */}
-      {/* <div className="w3-main w3-content w3-padding"> */}
-      {/* First Photo Grid */}
-      {/* <div className="w3-row-padding w3-padding-16 w3-center" id="food">
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/sandwich.jpg"
-              alt="Sandwich"
-            />
-            <h3>The Perfect Sandwich, A Real NYC Classic</h3>
-            <p>
-              Just some random text, lorem ipsum text praesent tincidunt ipsum
-              lipsum.
-            </p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/steak.jpg"
-              alt="Steak"
-            />
-            <h3>Let Me Tell You About This Steak</h3>
-            <p>
-              Once again, some random text to lorem lorem lorem lorem ipsum text
-              praesent tincidunt ipsum lipsum.
-            </p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/cherries.jpg"
-              alt="Cherries"
-            />
-            <h3>Cherries, interrupted</h3>
-            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-            <p>What else?</p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/wine.jpg"
-              alt="Pasta and Wine"
-            />
-            <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
-            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-          </div>
-        </div> */}
-
-      {/* Second Photo Grid */}
-      {/* <div className="w3-row-padding w3-padding-16 w3-center">
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/popsicle.jpg"
-              alt="Popsicle"
-            />
-            <h3>All I Need Is a Popsicle</h3>
-            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/salmon.jpg"
-              alt="Salmon"
-            />
-            <h3>Salmon For Your Skin</h3>
-            <p>
-              Once again, some random text to lorem lorem lorem lorem ipsum text
-              praesent tincidunt ipsum lipsum.
-            </p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/sandwich.jpg"
-              alt="Sandwich"
-            />
-            <h3>The Perfect Sandwich, A Real Classic</h3>
-            <p>
-              Just some random text, lorem ipsum text praesent tincidunt ipsum
-              lipsum.
-            </p>
-          </div>
-          <div className="w3-quarter">
-            <img
-              src="https://www.w3schools.com/w3images/croissant.jpg"
-              alt="Croissant"
-            />
-            <h3>Le French</h3>
-            <p>
-              Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum
-              lipsum.
-            </p>
-          </div>
-        </div> */}
-
-      {/* Pagination */}
-      {/* <div className="w3-center w3-padding-32">
-          <div className="w3-bar">
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              «
+      <nav aria-label="Page navigation example ">
+        <ul className="pagination justify-content-center align-content-center">
+          <li className="page-item">
+            <a className="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">«</span>
+              <span className="sr-only">Previous</span>
             </a>
-            <a href="#" className="w3-bar-item w3-black w3-button">
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
               1
             </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
               2
             </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
               3
             </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              4
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">»</span>
+              <span className="sr-only">Next</span>
             </a>
-            <a href="#" className="w3-bar-item w3-button w3-hover-black">
-              »
-            </a>
-          </div>
-        </div>
-
-        <hr id="about" /> */}
-
-      {/* About Section */}
-      {/* <div className="w3-container w3-padding-32 w3-center">
-          <h3>About Me, The Food Man</h3>
-          <br />
-          <img
-            src="https://www.w3schools.com/w3images/chef.jpg"
-            alt="Me"
-            className="w3-image"
-            style={{ display: "block", margin: "auto" }}
-            width="800"
-            height="533"
-          />
-          <div className="w3-padding-32">
-            <h4>
-              <b>I am Who I Am!</b>
-            </h4>
-            <h6>
-              <i>With Passion For Real, Good Food</i>
-            </h6>
-            <p>
-              Just me, myself and I, exploring the universe of unknownment. I
-              have a heart of love and an interest of lorem ipsum and mauris
-              neque quam blog. I want to share my world with you.Praesent
-              tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta
-              lectus vitae, ultricies congue gravida diam non fringilla.
-              Praesent tincidunt sed tellus ut rutrum. Sed vitae justo
-              condimentum, porta lectus vitae, ultricies congue gravida diam non
-              fringilla.
-            </p>
-          </div>
-        </div>
-        <hr /> */}
+          </li>
+        </ul>
+      </nav>
 
       {/* Footer */}
       <footer className="w3-row-padding w3-padding-32">
@@ -252,7 +106,7 @@ function Blog() {
           </p>
         </div>
 
-        <div className="w3-third">
+        {/* <div className="w3-third">
           <h3>BLOG POSTS</h3>
           <ul className="w3-ul w3-hoverable">
             <li className="w3-padding-16">
@@ -276,7 +130,7 @@ function Blog() {
               <span>Praes tinci sed</span>
             </li>
           </ul>
-        </div>
+        </div> */}
         <div className="w3-third w3-serif">
           <h3>POPULAR TAGS</h3>
           <p>
