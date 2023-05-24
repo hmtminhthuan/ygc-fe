785eced5a39f23aa563f2e82e732a47d8b156c69
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-25 03:45:50 +0700

    upload

--- src/pages/Courses/CourseDetail/CourseFeedback/CourseFeedback.jsx
+++ src/pages/Courses/CourseDetail/CourseFeedback/CourseFeedback.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import maleImg from "../../../../assets/images/avt-male.jpg";
 import femaleImg from "../../../../assets/images/avt-female.jpg";
 import axios from "axios";
 import "./CourseFeedback.scss";
 export default function CourseFeedback({ courseFeedback, ...restParams }) {
-    const getTraineeInfo = async (id) => {
-        await axios
-            .get("http://localhost:5000/Feedback/GetFeedbackDetailForTrainee", {
-                params: {
-                    feedbackId: id,
-                },
-            })
-            .then((res) => {
-                return res.data;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+    let traineeInfoArr = [];
+    let [traineeInfo, setTraineeInfo] = useState([]);
+    let [count, setCount] = useState(0);
+
+    useEffect(() => {
+        courseFeedback.forEach(({ id, ...restParams }) => {
+            axios
+                .get("http://localhost:5000/Feedback/GetFeedbackDetailForTrainee", {
+                    params: {
+                        feedbackId: id,
+                    },
+                })
+                .then((res) => {
+                    traineeInfoArr.push({ ...res.data, id });
+                    if (traineeInfoArr.length == courseFeedback.length) {
+                        setTraineeInfo(traineeInfoArr);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+        if (courseFeedback.length <= 0) {
+            setCount((prevCount) => prevCount + 1);
+        } else {
+            setCount(0);
+        }
+    }, [count]);
 
     return (
         <div className="row flex align-items-center justify-content-center">
@@ -44,13 +58,15 @@ export default function CourseFeedback({ courseFeedback, ...restParams }) {
                                 },
                                 index
                             ) => {
+                                let pos = traineeInfo.findIndex(obj => { return obj.id == id });
                                 return (
                                     <div
                                         className="row m-3 mx-0 mx-lg-5 mt-0 flex justify-content-center align-items-start"
                                         key={index}
                                     >
-                                        <div className="col-2 flex justify-content-end align-items-start">
+                                        <div className="col-2 flex justify-content-end align-items-start px-3">
                                             <img
+                                                className="feedback-trainee-img"
                                                 style={{
                                                     width: "50px",
                                                     height: "50px",
@@ -60,7 +76,15 @@ export default function CourseFeedback({ courseFeedback, ...restParams }) {
                                             />
                                         </div>
                                         <div className="col-9">
-                                            <p className="p-0 m-0">
+                                            {traineeInfo.length == courseFeedback.length ? (
+                                                <p className="p-0 m-0" style={{ fontWeight: "600" }}>
+                                                    <span> {traineeInfo[pos].firstname}</span>{" "}
+                                                    <span>  {traineeInfo[pos].lastname}</span>{" "}
+                                                </p>
+                                            ) : (
+                                                <></>
+                                            )}
+                                            <p className="p-0 mt-2">
                                                 <span style={{ color: "#e47200", fontWeight: "500" }}>
                                                     Rating:
                                                 </span>{" "}
@@ -85,7 +109,8 @@ export default function CourseFeedback({ courseFeedback, ...restParams }) {
                                                         }`}
                                                 ></i>
                                             </p>
-                                            <p className="p-0 m-0 mt-1">{description}</p>
+
+                                            <p className="p-0 m-0">{description}</p>
                                         </div>
                                         <hr className="mt-4" />
                                     </div>
