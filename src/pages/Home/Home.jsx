bfd060cb04970d9e4327ee299668a2b7c6cbf5e1
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-25 21:09:37 +0700

    upload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeaderHome from '../../component/HeaderHome/HeaderHome'
 import FooterHome from '../../component/FooterHome/FooterHome'
 import HomeCarousel from '../../templates/HomeTemplate/HomeCarousel/HomeCarousel'
+import HomeContent from '../../component/HomeContent/HomeContent'
 export default function Home() {
 
     return (
         <div>
             <HeaderHome />
             <HomeCarousel />
+            <HomeContent />
             <FooterHome />
         </div>
     )
