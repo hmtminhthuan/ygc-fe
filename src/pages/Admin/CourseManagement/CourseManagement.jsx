6bbc744365ee6c639ae0a3975d2709ebd4915b6d
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-05-29 21:37:05 +0700

    upload

--- src/pages/Admin/CourseManagement/CourseManagement.jsx
+++ src/pages/Admin/CourseManagement/CourseManagement.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import HeaderAdmin from '../../../component/Admin/HeaderAdmin/HeaderAdmin'
-import MeanuAdmin from '../../../component/Admin/MenuAdmin/MeanuAdmin'
+import MenuAdmin from '../../../component/Admin/MenuAdmin/MenuAdmin'
 import './CourseManagement.scss'
 export default function CourseManagement() {
     return (
         <>
             <HeaderAdmin />
             <section className="main" id="admin-course-management-area">
-                <MeanuAdmin />
+                <MenuAdmin />
 
             </section>
         </>
