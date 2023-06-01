886954c6b30e8fbe4c1d08cd5c6390f2a0310050
NgocTram <tramngoc3402@gmail.com>
2023-06-01 17:12:09 +0700

    upp

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -27,6 +27,7 @@ import FeedbackManagement from "../pages/Dashboard/Staff/Feedback/FeedbackManage
 import AdminCourseCreate from "../pages/Admin/CourseManagement/AdminCourseCreate/AdminCourseCreate";
 import ListStaff from "../pages/Admin/StaffManagement/ListStaff";
 import UserProfile from "../pages/UserProfile";
+import AdminCourseEdit from "../pages/Admin/CourseManagement/AdminCourseEdit/AdminCourseEdit";
 
 export default function Routers() {
   const routing = useRoutes([
