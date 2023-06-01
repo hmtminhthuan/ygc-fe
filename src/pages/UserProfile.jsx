17071c056ad021600214bb7fd7df7a138e87490e
NgocTram <tramngoc3402@gmail.com>
2023-06-01 17:10:23 +0700

    update user

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,6 +4,7 @@ import "./UserProfile.scss";
 import video from "../assets/video.mp4";
 import "remixicon/fonts/remixicon.css";
 import { api } from "../constants/api";
+import { Link } from "react-router-dom";
 
 function UserProfile() {
   const param = useParams();
@@ -21,8 +22,9 @@ function UserProfile() {
         console.log(err);
       });
   }, [param.id]);
+
   if (profile === null || profile[0] == null || profile[0] == undefined) {
-    return null; // Render loading state or handle the case when blogDetail is not available yet
+    return null;
   }
 
   let {
@@ -35,6 +37,17 @@ function UserProfile() {
     role,
     ...restParams
   } = profile[0];
+
+  let roleColor = "";
+  if (role.name === "Admin") {
+    roleColor = "text-red";
+  } else if (role.name === "Staff") {
+    roleColor = "text-orange";
+  } else if (role.name === "Trainer") {
+    roleColor = "text-blue";
+  } else if (role.name === "Trainee") {
+    roleColor = "text-green";
+  }
   return (
     <div className="page-content page-container " id="page-content">
       <div className="video-background">
@@ -46,7 +59,7 @@ function UserProfile() {
             <div className="card user-card-full ">
               <div className="row m-l-0 m-r-0 ">
                 <div className="col-sm-4 bg-c-lite-green user-profile">
-                  <div className="card-block text-center text-grey">
+                  <div className="card-block text-center">
                     <div className="m-b-25">
                       <img
                         src="https://img.icons8.com/bubbles/100/000000/user.png"
@@ -54,20 +67,36 @@ function UserProfile() {
                         alt="User-Profile-Image"
                       />
                     </div>
-                    <h6 className="f-w-600">
+                    <h6 className="f-w-600" style={{ fontSize: "25px" }}>
                       {firstName} {lastName}
                     </h6>
-                    <p>Role: {role.name} </p>
+                    <p
+                      className={`${roleColor}`}
+                      style={{ fontSize: "16px", fontWeight: "bold" }}
+                    >
+                      Role: {role.name}
+                    </p>
                     <div className="icon mt-3">
-                      <i className="ri-home-4-line mt-5 mx-3 "></i>
-                      <i className="ri-edit-2-line mt-5  mx-3 "></i>
+                      <Link to={"/"} className="home">
+                        <i className="ri-home-4-line mt-5 mx-3 "></i>
+                      </Link>
+
+                      <Link
+                        to={`/updateProfile/${profile.id}`}
+                        className="updateInfo"
+                      >
+                        <i className="ri-edit-2-line mt-5  mx-3 "></i>
+                      </Link>
                     </div>
                   </div>
                 </div>
                 <div className="col-sm-8">
                   <div className="card-block my-2 mx-4">
-                    <h6 className="m-b-20 p-b-5 b-b-default f-w-600">
-                      Information
+                    <h6
+                      className="m-b-20 p-b-5 b-b-default f-w-600"
+                      style={{ color: "rgba(210, 145, 188, 1)" }}
+                    >
+                      INFORMATION
                     </h6>
                     <div className="row">
                       <div className="col-sm-6 mb-3">
@@ -82,82 +111,34 @@ function UserProfile() {
                       </div>
                       <div className="col-sm-6 mb-3">
                         <p className="m-b-10 f-w-600">Phone</p>
-                        <h6 className="text-muted f-w-400">
-                          {phoneNumber}
-                        </h6>
+                        <h6 className="text-muted f-w-400">{phoneNumber}</h6>
                       </div>
 
                       <div className="col-sm-6 mb-3">
                         <p className="m-b-10 f-w-600">Address</p>
-                        <h6 className="text-muted f-w-400">
-                          {address}
-                        </h6>
+                        <h6 className="text-muted f-w-400">{address}</h6>
                       </div>
                     </div>
-                    <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">
-                      Course
+                    <h6
+                      className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600"
+                      style={{ color: "rgba(210, 145, 188, 1)" }}
+                    >
+                      COURSE
                     </h6>
                     <div className="row">
                       <div className="col-sm-6 mb-3">
                         <p className="m-b-10 f-w-600">Course name current</p>
-                        <h6 className="text-muted f-w-400">Sam Disuja</h6>
+                        <h6 className="text-muted f-w-400">none</h6>
                       </div>
                       <div className="col-sm-6 mb-3">
                         <p className="m-b-10 f-w-600">Class</p>
-                        <h6 className="text-muted f-w-400">Dinoter husainm</h6>
+                        <h6 className="text-muted f-w-400">none</h6>
                       </div>
                       <div className="col-sm-6 ">
                         <p className="m-b-10 f-w-600">Level</p>
-                        <h6 className="text-muted f-w-400">Dinoter husainm</h6>
+                        <h6 className="text-muted f-w-400">none</h6>
                       </div>
                     </div>
-                    <ul className="social-link list-unstyled m-t-40 m-b-10">
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="facebook"
-                          data-abc="true"
-                        >
-                          <i
-                            className="mdi mdi-facebook feather icon-facebook facebook"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="twitter"
-                          data-abc="true"
-                        >
-                          <i
-                            className="mdi mdi-twitter feather icon-twitter twitter"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#!"
-                          data-toggle="tooltip"
-                          data-placement="bottom"
-                          title=""
-                          data-original-title="instagram"
-                          data-abc="true"
-                        >
-                          <i
-                            className="mdi mdi-instagram feather icon-instagram instagram"
-                            aria-hidden="true"
-                          ></i>
-                        </a>
-                      </li>
-                    </ul>
                   </div>
                 </div>
               </div>
