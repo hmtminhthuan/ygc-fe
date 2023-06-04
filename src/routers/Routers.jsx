ddaee5a416e0bad9ad44e2596d30f1bca75fc70d
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-06-04 21:31:28 +0700

    update

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -48,7 +48,6 @@ export default function Routers() {
         { path: "/blogPage/:id", element: <BlogPage /> },
         { path: "/login", element: <Login /> },
         { path: "/register", element: <Register /> },
-        { path: "/dashboard", element: <Dashboard /> },
       ],
     },
     {
