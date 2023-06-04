21215647f86622176d12fcb3d258d7f14120bfe9
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-06-05 02:00:40 +0700

    update

--- src/pages/Courses/CourseDetail/CourseDetail.jsx
+++ src/pages/Courses/CourseDetail/CourseDetail.jsx
@@ -11,128 +11,235 @@ import CourseFeedback from "./CourseFeedback/CourseFeedback";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { api } from "../../../constants/api";
 import FooterHome from "../../../component/FooterHome/FooterHome";
+import Swal from "sweetalert2";
 
 export default function CourseDetail() {
-    const param = useParams();
-    const [courseDetail, setCourseDetail] = useState([]);
-    const [courseClasses, setCourseClasses] = useState([]);
-    const [courseFeedback, setCourseFeedback] = useState([]);
-    const formatPrice = (price) => {
-        return Intl.NumberFormat("vi-VN", {
-            style: "currency",
-            currency: "VND",
-        }).format(price);
-    };
-    useEffect(() => {
-        api
-            .get("/Course/GetCourseByID", {
-                params: { id: param.id },
-            })
-            .then((res) => {
-                setCourseDetail(res.data);
-            })
-            .catch((err) => {
-                // console.log(err);
-            });
+  const param = useParams();
+  const [courseDetail, setCourseDetail] = useState([]);
+  const [courseClasses, setCourseClasses] = useState([]);
+  const [courseFeedback, setCourseFeedback] = useState([]);
+  const [availablePayment, setAvailablePayment] = useState(false);
+  const [linkPayment, setLinkPayment] = useState("");
+  const [userLogin, setUserLogin] = useState({});
+  const formatPrice = (price) => {
+    return Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(price);
+  };
+  useEffect(() => {
+    api
+      .get("/Course/GetCourseByID", {
+        params: { id: param.id },
+      })
+      .then((res) => {
+        setCourseDetail(res.data);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
 
-        api
-            .get("/Class/GetClassByCourseID", {
-                params: { courseid: param.id },
-            })
-            .then((res) => {
-                setCourseClasses(res.data);
-            })
-            .catch((err) => {
-                // console.log(err);
-            });
+    api
+      .get("/Class/GetClassByCourseID", {
+        params: { courseid: param.id },
+      })
+      .then((res) => {
+        setCourseClasses(res.data);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
 
-        api
-            .get("/Feedback/GetCourseFeedbackbyId", {
-                params: { courseid: param.id },
-            })
-            .then((res) => {
-                setCourseFeedback(res.data);
-            })
-            .catch((err) => {
-                // console.log(err);
-            });
-    }, []);
-    let { courseName, levelName, description, price, discount, courseImg, ...restParams } =
-        courseDetail;
+    api
+      .get("/Feedback/GetCourseFeedbackbyId", {
+        params: { courseid: param.id },
+      })
+      .then((res) => {
+        setCourseFeedback(res.data);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
 
-    return (
-        <div>
-            <div className="header-top m-4 mx-0 mt-0">
-                <HeaderHome />
+    const USER_LOGIN = localStorage.getItem("USER_LOGIN");
+    let USER = {};
+    if (USER_LOGIN != null) {
+      USER = JSON.parse(USER_LOGIN);
+      setUserLogin(USER);
+      if (
+        !(
+          USER.accountID == null ||
+          USER.accountID == undefined ||
+          USER.role == null ||
+          USER.role == undefined ||
+          USER.role.id != 4
+        )
+      ) {
+        setAvailablePayment(true);
+        api
+          .post(`/CheckOutVNPAY`, {
+            amount: parseInt(price * (1 - discount / 100)),
+            accId: USER.accountID,
+            courseId: param.id,
+          })
+          .then((res) => {
+            setLinkPayment(res.data);
+          })
+          .catch((err) => {});
+      }
+    }
+  }, []);
+  let {
+    courseName,
+    levelName,
+    description,
+    price,
+    discount,
+    courseImg,
+    ...restParams
+  } = courseDetail;
+
+  return (
+    <div>
+      <div className="header-top m-4 mx-0 mt-0">
+        <HeaderHome />
+      </div>
+      <main className="pt-5">
+        <div className="box course-detail-area mt-5 my-5">
+          <div className="course-detail-info w-100 form-container flex-column justify-content-start align-items-start p-3">
+            <div className="row justify-content-center">
+              <div className="col-10 mt-3">
+                <Link
+                  to={"/course"}
+                  className="course-detail-come-back text-dark text-center text-decoration-none flex align-items-center"
+                  style={{ fontSize: "18px", fontWeight: "500" }}
+                >
+                  <i className="fa-solid fa-arrow-left"></i>
+                  <span className="mx-2">Back</span>
+                </Link>
+              </div>{" "}
             </div>
-            <main className="pt-5">
-                <div className="box course-detail-area mt-5 my-5">
-                    <div className="course-detail-info w-100 form-container flex-column justify-content-start align-items-start p-3">
-                        <div className="row justify-content-center">
-                            <div className="col-10 mt-3">
-                                <Link to={"/course"} className="course-detail-come-back text-dark text-center text-decoration-none flex align-items-center"
-                                    style={{ "fontSize": "18px", "fontWeight": "500" }}>
-                                    <i className="fa-solid fa-arrow-left"></i>
-                                    <span className="mx-2">Back</span>
-                                </Link>
-                            </div>{" "}
-                        </div>
-                        <h2 className="course-detail-title course-detail-name m-4 mt-0">{courseName}</h2>
-                        <div className="row d-lg-flex align-items-stretch justify-content-center">
-                            <div className="course-detail-img col-lg-5 col-10 flex justify-content-start">
-                                <img className="h-100" style={{ width: "100%" }} src={courseImg} />
-                                {discount != null && discount != "" && discount >= 0 ? (
-                                    <div className="course-tag-discount py-2 pt-1 px-2">
-                                        <p>{discount}&#37;</p>
-                                        <p>Discount</p>
-                                    </div>
-                                ) : (
-                                    <></>
-                                )}
-                            </div>
-                            <div className="course-detail-des col-lg-5 col-10 mt-lg-0 mt-sm-3 mt-sm-4 px-4 h-10">
-                                <p className="course-detail-des-level">
-                                    {" "}
-                                    <span className="sub-title">Level: </span> {levelName}
-                                </p>
-                                <p className="course-detail-des-description">
-                                    <span className="sub-title">Description: </span>
-                                    {description}
-                                </p>
-                                <div className="course-detail-des-price d-flex">
-                                    <span className="sub-title" style={{ margin: "0 10px 0 0" }}>
-                                        Price:
-                                    </span>
-                                    {discount != null && discount != "" && discount >= 0 ? (
-                                        <div className="d-lg-flex">
-                                            <p className="course-price-before-discount p-0">
-                                                {formatPrice(price)}
-                                            </p>
-                                            <p className="course-price-after-discount mx-lg-2 p-0">
-                                                {formatPrice(price - (price * discount) / 100)}
-                                            </p>
-                                        </div>
-                                    ) : (
-                                        <p className="course-price course-price-after-discount">{formatPrice(price)} </p>
-                                    )}
-                                </div>
-                                <div className="flex justify-content-center mt-2">
-                                    <Button className="w-75 flex">Register Now</Button>
-                                </div>
-                            </div>
-                        </div>
-                        <h2 className="sub-title course-detail-title mt-4">
-                            Available Classes
-                        </h2>
-                        <CourseClasses courseClasses={courseClasses} />
-                        <h2 className="sub-title course-detail-title mt-4">Rating &amp; Feedbacks</h2>
-                        <CourseFeedback courseFeedback={courseFeedback} />
+            <h2 className="course-detail-title course-detail-name m-4 mt-0">
+              {courseName}
+            </h2>
+            <div className="row d-lg-flex align-items-stretch justify-content-center">
+              <div className="course-detail-img col-lg-5 col-10 flex justify-content-start">
+                <img
+                  className="h-100"
+                  style={{ width: "100%" }}
+                  src={courseImg}
+                />
+                {discount != null && discount != "" && discount >= 0 ? (
+                  <div className="course-tag-discount py-2 pt-1 px-2">
+                    <p>{discount}&#37;</p>
+                    <p>Discount</p>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div className="course-detail-des col-lg-5 col-10 mt-lg-0 mt-sm-3 mt-sm-4 px-4 h-10">
+                <p className="course-detail-des-level">
+                  {" "}
+                  <span className="sub-title">Level: </span> {levelName}
+                </p>
+                <p className="course-detail-des-description">
+                  <span className="sub-title">Description: </span>
+                  {description}
+                </p>
+                <div className="course-detail-des-price d-flex">
+                  <span className="sub-title" style={{ margin: "0 10px 0 0" }}>
+                    Price:
+                  </span>
+                  {discount != null && discount != "" && discount >= 0 ? (
+                    <div className="d-lg-flex">
+                      <p className="course-price-before-discount p-0">
+                        {formatPrice(price)}
+                      </p>
+                      <p className="course-price-after-discount mx-lg-2 p-0">
+                        {formatPrice(price - (price * discount) / 100)}
+                      </p>
                     </div>
+                  ) : (
+                    <p className="course-price course-price-after-discount">
+                      {formatPrice(price)}{" "}
+                    </p>
+                  )}
                 </div>
-            </main >
-            <div>
-                <FooterHome />
+                <div className="flex justify-content-center mt-2">
+                  {availablePayment ? (
+                    <Button
+                      href={linkPayment}
+                      target="blank"
+                      className="w-75 flex"
+                      variant=""
+                    >
+                      <p
+                        className="p-0 m-0 bg-success text-light py-2 bg-opacity-100"
+                        style={{ borderRadius: "8px", fontWeight: "bolder" }}
+                      >
+                        Register Now
+                      </p>
+                    </Button>
+                  ) : (
+                    <button
+                      className="w-75 flex justify-content-center text-cente py-2 border-0"
+                      style={{ borderRadius: "8px" }}
+                      onClick={() => {
+                        if (
+                          userLogin.role == null ||
+                          userLogin.role == undefined
+                        ) {
+                          Swal.fire({
+                            title: `You need to Log in first`,
+                            html: `Do you want to Log in now?`,
+                            icon: "info",
+                            showCancelButton: true,
+                            showConfirmButton: true,
+                            confirmButtonText: "Yes",
+                            cancelButtonText: "No",
+                            allowOutsideClick: false,
+                          }).then((result) => {
+                            if (
+                              result.isDenied === true ||
+                              result.isDismissed === true
+                            ) {
+                            } else if (result.isConfirmed === true) {
+                              window.location.href = "/login";
+                            }
+                          });
+                        } else if (userLogin.role.id != 4) {
+                          Swal.fire({
+                            title: `Your account cannot register course.`,
+                            icon: "info",
+                            showCancelButton: false,
+                            showConfirmButton: true,
+                            confirmButtonText: "Confirm",
+                            allowOutsideClick: true,
+                          });
+                        }
+                      }}
+                    >
+                      Register Now
+                    </button>
+                  )}
+                </div>
+              </div>
             </div>
-        </div >
-    );
+            <h2 className="sub-title course-detail-title mt-4">
+              Available Classes
+            </h2>
+            <CourseClasses courseClasses={courseClasses} />
+            <h2 className="sub-title course-detail-title mt-4">
+              Rating &amp; Feedbacks
+            </h2>
+            <CourseFeedback courseFeedback={courseFeedback} />
+          </div>
+        </div>
+      </main>
+      <div>
+        <FooterHome />
+      </div>
+    </div>
+  );
 }
--- src/pages/Courses/CourseItem.jsx
+++ src/pages/Courses/CourseItem.jsx
@@ -1,92 +1,172 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import "./CourseItem.scss";
 import { Link } from "react-router-dom";
 import image from "../../assets/images/img-demo.jpg";
+import { api } from "../../constants/api";
+import Swal from "sweetalert2";
 export default function CourseDetail({
-    courseID,
-    courseName,
-    description,
-    levelName,
-    price,
-    discount,
-    courseImg,
-    ...restParams
+  courseID,
+  courseName,
+  description,
+  levelName,
+  price,
+  discount,
+  courseImg,
+  ...restParams
 }) {
-    const formatPrice = (price) => {
-        return Intl.NumberFormat("vi-VN", {
-            style: "currency",
-            currency: "VND",
-        }).format(price);
-    };
-    return (
-        <div className="col-lg-4 col-sm-12 col-md-6 flex justify-content-center">
-            <Card style={{ width: "85%" }} className="my-4">
-                <div className="w-100 text-center" style={{ height: "250px" }}>
-                    <Card.Img
-                        variant="top"
-                        src={courseImg == null || courseImg == "" ? image : courseImg}
-                    />
-                </div>
-                {discount != null && discount != "" && discount >= 0 ? (
-                    <div className="course-tag-discount py-2 pt-1 px-2">
-                        <p>{discount}&#37;</p>
-                        <p>Discount</p>
-                    </div>
-                ) : (
-                    <></>
-                )}
-                <Card.Body>
-                    <p className="course-level my-2 mt-2"> Level: {levelName}</p>
-                    <Card.Title>{courseName}</Card.Title>
-                    <Card.Text className="course-des">
-                        {description.length >= 100
-                            ? description.substring(0, 100).trim() + "..."
-                            : description}{" "}
-                        {description.length >= 100 ? (
-                            <Link
-                                to={`/courseDetail/${courseID}`}
-                                className="text-decoration-none"
-                            >
-                                View More
-                            </Link>
-                        ) : (
-                            <></>
-                        )}
-                    </Card.Text>
-                    {discount != null && discount != "" && discount >= 0 ? (
-                        <p className="my-2" style={{ fontWeight: "600" }}>
-                            <span className="course-price-before-discount p-0">
-                                {formatPrice(price)}
-                            </span>
-                            <span className="course-price-after-discount mx-2 p-0">
-                                {formatPrice(price - (price * discount) / 100)}
-                            </span>
-                        </p>
-                    ) : (
-                        <p className="course-price my-2" style={{ fontWeight: "600" }}>
-                            {formatPrice(price)}{" "}
-                        </p>
-                    )}
+  const [availablePayment, setAvailablePayment] = useState(false);
+  const [linkPayment, setLinkPayment] = useState("");
+  const [userLogin, setUserLogin] = useState({});
+  const formatPrice = (price) => {
+    return Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(price);
+  };
 
-                    <div className="button text-center pt-1">
-                        <Button
-                            href={`/courseDetail/${courseID}`}
-                            className="course-view-detail mx-1 mt-1"
-                            variant=""
-                        >
-                            View Details
-                        </Button>
-                        <Button
-                            href=""
-                            className="course-register-now mx-1 mt-1"
-                            variant=""
-                        >
-                            Register Now
-                        </Button>
-                    </div>
-                </Card.Body>
-            </Card>
+  useEffect(() => {
+    const USER_LOGIN = localStorage.getItem("USER_LOGIN");
+    let USER = {};
+    if (USER_LOGIN != null) {
+      USER = JSON.parse(USER_LOGIN);
+      setUserLogin(USER);
+      if (
+        !(
+          USER.accountID == null ||
+          USER.accountID == undefined ||
+          USER.role == null ||
+          USER.role == undefined ||
+          USER.role.id != 4
+        )
+      ) {
+        setAvailablePayment(true);
+        api
+          .post(`/CheckOutVNPAY`, {
+            amount: parseInt(price * (1 - discount / 100)),
+            accId: USER.accountID,
+            courseId: courseID,
+          })
+          .then((res) => {
+            setLinkPayment(res.data);
+          })
+          .catch((err) => {});
+      }
+    }
+  }, []);
+
+  return (
+    <div className="col-lg-4 col-sm-12 col-md-6 flex justify-content-center">
+      <Card style={{ width: "85%" }} className="my-4">
+        <div className="w-100 text-center" style={{ height: "250px" }}>
+          <Card.Img
+            variant="top"
+            src={courseImg == null || courseImg == "" ? image : courseImg}
+          />
         </div>
-    );
+        {discount != null && discount != "" && discount >= 0 ? (
+          <div className="course-tag-discount py-2 pt-1 px-2">
+            <p>{discount}&#37;</p>
+            <p>Discount</p>
+          </div>
+        ) : (
+          <></>
+        )}
+        <Card.Body>
+          <p className="course-level my-2 mt-2"> Level: {levelName}</p>
+          <Card.Title>{courseName}</Card.Title>
+          <Card.Text className="course-des">
+            {description.length >= 100
+              ? description.substring(0, 100).trim() + "..."
+              : description}{" "}
+            {description.length >= 100 ? (
+              <Link
+                to={`/courseDetail/${courseID}`}
+                className="text-decoration-none"
+              >
+                View More
+              </Link>
+            ) : (
+              <></>
+            )}
+          </Card.Text>
+          {discount != null && discount != "" && discount >= 0 ? (
+            <p className="my-2" style={{ fontWeight: "600" }}>
+              <span className="course-price-before-discount p-0">
+                {formatPrice(price)}
+              </span>
+              <span className="course-price-after-discount mx-2 p-0">
+                {formatPrice(price - (price * discount) / 100)}
+              </span>
+            </p>
+          ) : (
+            <p className="course-price my-2" style={{ fontWeight: "600" }}>
+              {formatPrice(price)}{" "}
+            </p>
+          )}
+
+          <div className="button text-center pt-1">
+            <Button
+              href={`/courseDetail/${courseID}`}
+              className="course-view-detail mx-1 mt-1"
+              variant=""
+            >
+              View Details
+            </Button>
+            {availablePayment ? (
+              <Button
+                href={linkPayment}
+                className="course-register-now mx-1 mt-1"
+                variant=""
+                target="blank"
+              >
+                Register Now
+              </Button>
+            ) : (
+              <button
+                className="course-register-now m-0 mx-1 mt-1 border-0
+                py-2 px-2"
+                variant=""
+                target="blank"
+                style={{ borderRadius: "5px", transform: "translateY(2px)" }}
+                onClick={() => {
+                  if (userLogin.role == null || userLogin.role == undefined) {
+                    Swal.fire({
+                      title: `You need to Log in first`,
+                      html: `Do you want to Log in now?`,
+                      icon: "info",
+                      showCancelButton: true,
+                      showConfirmButton: true,
+                      confirmButtonText: "Yes",
+                      cancelButtonText: "No",
+                      allowOutsideClick: false,
+                    }).then((result) => {
+                      if (
+                        result.isDenied === true ||
+                        result.isDismissed === true
+                      ) {
+                      } else if (result.isConfirmed === true) {
+                        window.location.href = "/login";
+                      }
+                    });
+                  } else if (userLogin.role.id != 4) {
+                    Swal.fire({
+                      title: `Your account cannot register course.`,
+                      icon: "info",
+                      showCancelButton: false,
+                      showConfirmButton: true,
+                      confirmButtonText: "Confirm",
+                      allowOutsideClick: true,
+                    });
+                  }
+                }}
+              >
+                Register Now
+              </button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
