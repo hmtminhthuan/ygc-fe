70643115e0f909869646863c419c27e0dbb7301a
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-06-05 00:14:43 +0700

    update

--- src/templates/AdminTemplate/AdminTemplate.jsx
+++ src/templates/AdminTemplate/AdminTemplate.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router';
+import React from "react";
+import { Outlet } from "react-router";
 
 export default function AdminTemplate() {
-    const USER_LOGIN = localStorage.getItem("USER_LOGIN");
-    let USER = {};
-    USER = JSON.parse(USER_LOGIN);
-    if (USER_LOGIN == null || USER_LOGIN == undefined || !(USER.role.id == 1)) {
-        window.location.href = "/";
-    }
-    return (
-        <>
-            {!(USER.role.id == 1) ? (
-                <></>
-            ) : (
-                <div style={{}}>
-                    <Outlet />
-                </div>
-            )}
-        </>
-    );
+  const USER_LOGIN = localStorage.getItem("USER_LOGIN");
+  let USER = {};
+  USER = JSON.parse(USER_LOGIN);
+  if (USER_LOGIN == null || USER_LOGIN == undefined || !(USER.role.id == 1)) {
+    window.location.href = "/";
+  }
+  return (
+    <>
+      {USER_LOGIN == null || USER_LOGIN == undefined || !(USER.role.id == 1) ? (
+        <></>
+      ) : (
+        <div style={{}}>
+          <Outlet />
+        </div>
+      )}
+    </>
+  );
 }
