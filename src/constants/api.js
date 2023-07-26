21c484374e03454d403b82f63f386fba6005ab34
NgocTram <tramngoc3402@gmail.com>
2023-07-26 14:42:14 +0700

    update

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,21 +1,12 @@
 import axios from "axios";
 const baseURL = "https://yogacentermanagement.azurewebsites.net";
 
-export const api = axios
-  .create
-  //   {
-  //   baseURL,
-  // }
-  ();
+export const api = axios.create();
 
 api.interceptors.request.use((config) => {
   config = {
     ...config,
     baseURL,
-    headers: {
-      ...config.headers,
-      "Access-Control-Allow-Origin": "*", // Thêm tiêu đề CORS vào headers
-    },
   };
   return config;
 });
